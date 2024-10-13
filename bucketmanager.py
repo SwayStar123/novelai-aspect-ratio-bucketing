ec66fda7c95ff222cc8b10bacb84dcc10d9b1297
SwayStar123 <46050679+SwayStar123@users.noreply.github.com>
2024-10-13 19:56:14 +0530

    Update bucketmanager.py

--- bucketmanager.py
+++ bucketmanager.py
@@ -10,7 +10,7 @@ def get_prng(seed):
 
 class BucketManager:
     def __init__(self, bucket_file, valid_ids=None, max_size=(768,512), divisible=64, step_size=8, min_dim=256, base_res=(512,512), bsz=64, world_size=1, global_rank=0, max_ar_error=4, seed=42, dim_limit=1024, debug=False):
-        with open(bucket_file, "r") as fh:
+        with open(bucket_file, "rb") as fh:
             self.res_map = json.load(fh)
         if valid_ids is not None:
             new_res_map = {}
